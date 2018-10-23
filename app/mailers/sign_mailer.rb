2bcefbda8ffb325a75bb32683b57b9d88bab2361
dali <dalikim@parti.xyz>
2018-10-23 10:45:22 +0900

    버그 수정: 서명한 사람에게 메일 발송 시 로그인해서 서명한 경우나 실명 서명일 경우 메일에 받는 분 이름이 표시되지 않는 버그를 바로 잡습니다

--- app/mailers/sign_mailer.rb
+++ app/mailers/sign_mailer.rb
@@ -2,7 +2,7 @@ class SignMailer < ApplicationMailer
   def by_campaigner(sign, title, body)
     @sign = sign
     @body = body
-    mail(to: sign.signer_email, subject: title,
+    mail(to: sign.user_email, subject: title,
       template_name: "by_campaigner")
   end
 end
