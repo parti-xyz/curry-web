e350dd9cf32722eb9dca36de87ac9e88608cf089
dalikim <dalikim@parti.xyz>
2019-09-18 20:20:41 +0900

    웹주소와 메일 주소를 캠페인즈로 변경합니다

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   helper :application # gives access to all helpers defined within `application_helper`.
-  default from: "contact@govcraft.org"
+  default from: "contact@campaigns.kr"
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,8 +61,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.default_url_options = { :host => 'govcraft.org' }
-  config.action_mailer.asset_host =  'http://govcraft.org'
+  config.action_mailer.default_url_options = { :host => 'campaigns.kr' }
+  config.action_mailer.asset_host =  'http://campaigns.kr'
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.delivery_method = :aws_sdk
   # config.action_mailer.postmark_settings = {
@@ -92,4 +92,4 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
-Rails.application.routes.default_url_options[:host] = "govcraft.org"
+Rails.application.routes.default_url_options[:host] = "campaigns.kr"
