61683d7de3232638efcb7f38bb5a3eaf8de8699f
dalikim <dalikim@parti.xyz>
2019-11-07 10:17:36 +0900

    캠페인 시작 날짜가 저장되지 않는 버그를 바로 잡습니다

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -201,7 +201,7 @@ class CampaignsController < ApplicationController
 
   def campaign_params
     params.require(:campaign).permit(:title, :body, :project_id, :signs_goal_count, :cover_image, :thanks_mention,
-      :comment_enabled, :sign_title, :sign_placeholder, :social_image, :confirm_privacy,
+      :comment_enabled, :sign_title, :sign_placeholder, :social_image, :confirm_privacy, :opened_at,
       :use_signer_email, :use_signer_address, :use_signer_real_name, :use_signer_phone,
       :signer_email_title, :signer_address_title, :signer_real_name_title, :signer_phone_title,
       :agent_section_title, :agent_section_response_title, :sign_hidden, :area_id, :issue_id,
