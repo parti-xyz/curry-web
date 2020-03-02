431c3c89c5139f12807f9af63590df64ed3fa803
Swain Hwang <swain@swainuicbookpro.lan>
2020-03-02 00:27:20 -0800

    change confirm terms after login to interceptor

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def confirm_terms
+  end
+
+  def update
+    if current_user.update(user_terms_params)
+      redirect_to after_sign_up_path(resource)
+    else
+      render "users/confirm_terms"
+    end
+  end
+
+  private 
+  def user_terms_params
+    params.require(:user).permit(:term_service, :term_privacy, :term_marketing)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,6 @@ Rails.application.routes.draw do
   mount Redactor2Rails::Engine => '/redactor2_rails'
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     delete 'sign_out', to: 'devise/sessions#destroy'
@@ -42,6 +41,7 @@ Rails.application.routes.draw do
   get 'privacy', to: "pages#privacy", as: 'privacy'
   get 'terms', to: "pages#terms", as: 'terms'
   get 'marketing', to: "pages#marketing", as: 'marketing'
+  get 'confirm_terms', to: "users#confirm_terms", as: 'users_confirm_terms'
 
   post 'simple_mail/callback' => 'simple_mail#callback'
 
