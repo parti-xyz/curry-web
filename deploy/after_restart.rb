cc7d8d0a64153733e5ac78c3d65220bb6584f032
dalikim <dalikim@parti.xyz>
2020-03-31 14:59:44 +0900

    기술적인 수정 - 배포할 때 사이트맵을 갱신합니다

--- deploy/after_restart.rb
+++ deploy/after_restart.rb
@@ -1,4 +1,5 @@
 on_app_servers do
   sudo "sleep 20s ; monit start all -g #{config.app}_sidekiq"
   run "bundle exec rake data:load_once_assembly_members"
+  run "bundle exec rake sitemap:refresh"
 end
