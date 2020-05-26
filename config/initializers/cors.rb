3e48f2d075687689035b719cdca64df11ffc026f
dalikim <dalikim@parti.xyz>
2020-05-26 13:13:57 +0900

    캠페인의 더미 위젯을 만듭니다

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,11 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
+Rails.application.config.middleware.insert_before 0, Rack::Cors, debug: (!Rails.env.production?), logger: (-> { Rails.logger }) do
   allow do
     origins '*'
-    resource '/timelines/*.json', headers: :any, methods: :get
+    resource '/timelines/*.json', headers: :any, methods: [:get]
+  end
+
+  allow do
+    origins '*'
+    resource '/campaigns/widget/*', headers: :any, methods: [:get]
   end
 end
