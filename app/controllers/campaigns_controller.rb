612a4ca4559598645544d2ee6f087bcd3f381cb2
dalikim <dalikim@parti.xyz>
2020-11-06 15:15:20 +0900

    fix 캠페인 커버 이미지가 삭제되지 않던 버그를 바로 잡습니다 #222

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -235,14 +235,21 @@ class CampaignsController < ApplicationController
   private
 
   def campaign_params
-    params.require(:campaign).permit(:title, :body, :project_id, :goal_count, :cover_image, :thanks_mention,
-      :comment_enabled, :sign_title, :sign_placeholder, :social_image, :confirm_third_party, :opened_at, :stealthily,
-      :use_signer_email, :use_signer_address, :use_signer_real_name, :use_signer_phone, :use_signer_country, :use_signer_city,
-      :signer_email_title, :signer_address_title, :signer_real_name_title,
-      :signer_country_title,  :signer_city_title, :signer_phone_title,
-      :agent_section_title, :agent_section_response_title, :sign_hidden, :area_id, :issue_id,
-      :special_slug, :sign_form_intro, (:template if params[:action] == 'create'), :slug, :title_to_agent, :message_to_agent,
-      :agent_id, :css, :need_stance, :ga_id)
+      params.require(:campaign).permit(
+        :title, :body, :project_id, :goal_count,
+        :cover_image, :cover_image_cache, :remove_cover_image,
+        :social_image, :social_image_cache, :remove_social_image,
+        :thanks_mention,
+        :comment_enabled, :sign_title, :sign_placeholder,
+        :confirm_third_party, :opened_at, :stealthily,
+        :use_signer_email, :use_signer_address, :use_signer_real_name, :use_signer_phone, :use_signer_country, :use_signer_city,
+        :signer_email_title, :signer_address_title, :signer_real_name_title,
+        :signer_country_title,  :signer_city_title, :signer_phone_title,
+        :agent_section_title, :agent_section_response_title, :sign_hidden, :area_id, :issue_id,
+        :special_slug, :sign_form_intro,
+        (:template if params[:action] == 'create'),
+        :slug, :title_to_agent, :message_to_agent,
+        :agent_id, :css, :need_stance, :ga_id)
   end
 
   def reset_meta_tags_for_show
