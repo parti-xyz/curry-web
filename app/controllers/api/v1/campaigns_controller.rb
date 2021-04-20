6e10974a3de203bdea5f726ed0c5141a232cfee3
wagurano <wagurano@gmail.com>
2021-04-20 22:11:48 +0900

    캠페인의 서명 가져오기 API 실행 결과 속성이름을 바꿉니다.

--- app/controllers/api/v1/campaigns_controller.rb
+++ app/controllers/api/v1/campaigns_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CampaignsController < ApplicationController
 
   def signs_recent
     campaign = Campaign.find(params[:id])
-    signs = campaign.signs.order('id desc').take(8).map{ |x| { signer_name: x.user_name, body: x.body, signed_at: x.created_at, sign_count: campaign.signs.where('id <= ?', x).count } }
+    signs = campaign.signs.recent.take(8).map{ |x| { signer_name: x.user_name, body: x.body, signed_at: x.created_at, sequence_no: campaign.signs.where('id <= ?', x).count } }
     render json: signs
   end
 
